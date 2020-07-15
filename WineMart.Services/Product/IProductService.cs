a94fb1b14958eb596746a9c9362ba4663861cb24
festus obi <fessy_wonder@yahoo.co.uk>
2020-07-15 18:03:52 +0200

    Added product service

---
+++ WineMart.Services/Product/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WineMart.Services.Product
+{
+    public interface IProductService
+    {
+        List<Data.Models.Product> GetProducts();
+        Data.Models.Product GetProduct(int id);
+        ServiceResponse<Data.Models.Product> CreateProduct(Data.Models.Product product);
+        ServiceResponse<Data.Models.Product> ArchiveProduct(int id);
+    }
+}
---
+++ WineMart.Services/Product/ProductService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WineMart.Data;
+
+namespace WineMart.Services.Product
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ServiceResponse<Data.Models.Product> ArchiveProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<Data.Models.Product> CreateProduct(Data.Models.Product product)
+        {
+            try
+            {
+                _db.Products.Add(product);
+
+                var newInventory = new Data.Models.Inventory
+                {
+                    Product = product,
+                    AvailableQuantity = 0,
+                    SupposedQuantity = 10
+                };
+
+                _db.ProductInventories.Add(newInventory);
+
+                _db.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Saved new product",
+                    IsSuccess = true
+                };
+            }
+
+            catch (Exception e)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = e.StackTrace,
+                    IsSuccess = false
+                };
+            }
+        }
+
+        public Data.Models.Product GetProduct(int id)
+        {
+            return _db.Products.Find(id);
+        }
+
+        public List<Data.Models.Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+    }
+}
